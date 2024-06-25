73996bb84458a890dfce1a86925e0ce4c2630f30
Gustavo Monzon <gusmonz73.gm@gmail.com>
2024-06-25 00:41:52 -0300

    sincronizar

---
+++ db/db.js
@@ -0,0 +1,16 @@
+const mysql = require ("mysql2");
+
+const connection = mysql.createConnection({
+    host: 'https://phpmyadmin.alwaysdata.com/phpmyadmin/index.php?route=/',
+    user: 'gmonzon73',
+    password: 'ceroseis.2024',
+    database: 'gmonzon73_ceroseis',
+});
+connection.connect((error) => {
+    if (error){
+        return console.error(error)
+    }
+    console.log("conectado");
+});
+module.exports = connection;
+ 
