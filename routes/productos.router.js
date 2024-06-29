a36380368d088b2a1439a8b51ee0d6ab840c1f60
Gustavo Monzon <gusmonz73.gm@gmail.com>
2024-06-29 20:55:56 -0300

    logica de rutas al controller3

--- routes/productos.router.js
+++ routes/productos.router.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const productos = [
-    {id:1, nombre:"producto 1", stock: 10},
-    {id:2, nombre:"producto 2", stock: 5},
-    {id:3, nombre:"producto 3", stock: 10},
-];
 const controller = require("../controllers/productos.controller");
 //  Prefijo: /productos
+
 router.get("/", controller.index);
 router.get("/:id", controller.show);
 router.post("/", controller.store);
