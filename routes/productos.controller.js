6de1735e64d605907d17d84c7e0898f022522e2a
Gustavo Monzon <gusmonz73.gm@gmail.com>
2024-06-29 20:45:10 -0300

    logica de rutas al controller

--- routes/productos.controller.js
+++ routes/productos.controller.js
@@ -21,12 +21,60 @@ const index = (req, res) => {
         }
 
         res.json(rows[0]);
-    });
-    
-};  
+    });    
+}; 
+const store = (req, res) => { 
+  const { nombre, precio, stock } = req.body;
 
-};
+  const sql = "INSERT INTO productos (nombre, stock, precio) VALUE (?, ?, ?)";
+  db.query(sql, [nombre, stock, precio], (error, result) => {
+      if (error) {
+          return res.status(500).json({error: "intente mas tarde"});          
+      }      
+      const producto = {...req.body, id: result.insertId };
+      res.status(201).json(producto); 
+    });   
+}; 
+const update = (req, res) => {
+    const { id } = req.params;
+    const { nombre, precio, stock } = req.body;
+
+    const sql = "UPDATE productos SET nombre = ?, precio = ?, stock = ? WHERE id = ?";
+     db.query(sql, [nombre, precio, stock, id], (error, result) => {
+      if (error) {
+          return res.status(500).json({error: "intente mas tarde"});          
+      }  
+      if (result.affectedRows == 0) {
+        return res.status(404).send({error; "no existe el producto"}); 
+      }   
+      const producto = {...req.body, ...req.params }; 
+      res.json(producto); 
+    });
+
+} 
+
+} 
+const destroy = (req, res) => { 
+    const { id } = req.params;
+
+    const sql = "DELETE FROM productos WHERE id = ?";
+    db.query(sql, [id], (error, result) => {
+      if (error) {
+          return res.status(500).json({error: "intente mas tarde"});          
+      }
+      if (result.affectedRows ==0) {
+          return res.status(404).send({ error: "no existe el producto"});
+      }
+
+      res.json({mensaje: "Producto eliminado"});
+  });    
+}; 
+
+
 module.exports = {
   index,
   show,
+  store,
+  update,
+  destroy,
 };
--- routes/productos.router.js
+++ routes/productos.router.js
@@ -10,43 +10,9 @@ const controller = require("../controllers/productos.controller");
 //  Prefijo: /productos
 router.get("/", controller.index);
 router.get("/:id", controller.show);
+router.post("/", controller.store);
 
-router.post("/", (req, res) => {
-   //console.log(req.body);
-   //res.send("POST");
-   
-   const producto = {
-    id: productos.length + 1,
-    nombre: req.body.nombre,
-    stock: req.body.stock,
-   };
-   productos.push(producto);
-   res.status(201).send(producto);
-});
-
-router.put("/:id",(req, res) => {
-    //console.log(req.params);
-    //console.log(req.body);
-
-    const producto = productos.find((elemento) => elemento.id == req.params.id);
-    if (!producto) {
-        return res.status(404).send({ error: "no existe el producto"})
-    }
-    producto.nombre = req.body.nombre;
-    producto.stock = req.body.stock;
-    res.send(producto);
-});
-router.delete("/:id", (req, res) => {
-    
-    console.log(req.params);
-    const producto = productos.find((elemento) => elemento.id == req.params.id);
-    
-    if (!producto) {
-        return res.status(404).send({ error: "no existe el producto"})
-    }
-    const productoIndex = productos.findIndex((elemento) => elemento.id == req.params.id);
-    productos.splice(productoIndex, 1)
-    res.send(producto);
-});
+router.put("/:id",controller.update);
+router.delete("/:id", controller.destroy);
 
 module.exports = router;
